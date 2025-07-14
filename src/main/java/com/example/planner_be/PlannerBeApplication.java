788b9665a099c1d20680fbbe73aae32c4813f6b5
kdhdd <kdso10@naver.com>
2025-07-14 17:54:18 +0900

    :tada: Start: Start New Project

---
+++ src/main/java/com/example/planner_be/PlannerBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.planner_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerBeApplication.class, args);
+    }
+
+}
