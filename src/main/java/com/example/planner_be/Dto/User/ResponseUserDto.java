c0a912d95817ed0905d1eecf5dd77f7f98f422a4
kdhdd <kdso10@naver.com>
2025-07-16 16:18:32 +0900

    :sparkles: 회원 본인 조회 추가

--- src/main/java/com/example/planner_be/Dto/User/ResponseUserDto.java
+++ src/main/java/com/example/planner_be/Dto/User/ResponseUserDto.java
@@ -1,6 +1,7 @@
 package com.example.planner_be.Dto.User;
 
 import com.example.planner_be.Model.User;
+import com.example.planner_be.Model.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,11 +12,13 @@ import lombok.Getter;
 public class ResponseUserDto {
     private String username;
     private String nickname;
+    private UserRole role;
 
     public static ResponseUserDto entityToDto(User user) {
         return ResponseUserDto.builder()
                 .username(user.getUsername())
                 .nickname(user.getNickname())
+                .role(user.getRole())
                 .build();
     }
 }
